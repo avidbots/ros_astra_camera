0379842f9b3f5d40227d83f249a274a123e5f811
Adel Fakih <adel.fakih@avidbots.com>
2019-05-14 12:40:59 -0400

    Fix bug in usb_mutex

--- src/astra_driver.cpp
+++ src/astra_driver.cpp
@@ -47,6 +47,7 @@
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <boost/thread/thread.hpp>
 #include <boost/interprocess/sync/named_mutex.hpp>
+#include <boost/interprocess/sync/scoped_lock.hpp>
 
 #define  MULTI_ASTRA 0 // Have to make sure no multiple cameras launching at the same time outside if it is 0
 namespace astra_wrapper
@@ -144,13 +145,8 @@ AstraDriver::AstraDriver(const ros::NodeHandle& n, const ros::NodeHandle& pnh) :
 	 	initDevice();
 	 }
 #else
-  namespace bi = boost::interprocess;
-  bi::named_mutex usb_mutex{bi::open_or_create, "usb_mutex"};
-  usb_mutex.lock();
 
   initDevice();
-
-  usb_mutex.unlock();
 
 #endif
   // Initialize dynamic reconfigure
@@ -967,6 +963,10 @@ void AstraDriver::initDevice()
       	continue;
       }
       #endif
+      namespace bi = boost::interprocess;
+      bi::named_mutex usb_mutex{bi::open_or_create, "usb_mutex"};
+      bi::scoped_lock<bi::named_mutex> lock(usb_mutex);
+
       device_ = device_manager_->getDevice(device_URI);
     }
     catch (const AstraException& exception)
