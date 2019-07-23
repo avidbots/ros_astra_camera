58590b6bdaa045576b80292144c46442c3e1d42e
Weilong Ye <weilong.ye@avidbots.com>
2019-07-23 12:08:12 -0400

    Parameterized sleep time in astra_frame_reader

--- src/multi_astra_driver.cpp
+++ src/multi_astra_driver.cpp
@@ -1,4 +1,5 @@
 #include "astra_camera/multi_astra_driver.h"
+#include "astra_camera/astra_frame_reader.h"
 #include <boost/make_shared.hpp>
 
 namespace astra_wrapper
@@ -14,6 +15,12 @@ MultiAstraDriver::MultiAstraDriver(ros::NodeHandle& n, ros::NodeHandle& pnh) :
   openni::Status rc = openni::OpenNI::initialize();
   if (rc != openni::STATUS_OK)
     THROW_OPENNI_EXCEPTION("Initialize failed\n%s\n", openni::OpenNI::getExtendedError());
+
+  double sleep_time_before_read = 0.;
+  double sleep_time_after_read = 0.;
+  pnh_.param("sleep_time_before_read", sleep_time_before_read, 10.); 
+  pnh_.param("sleep_time_after_read", sleep_time_after_read, 70.); 
+  AstraFrameReader::getSingleton()->SetSleepTime(sleep_time_before_read, sleep_time_after_read);
 
   astra_register_sub_ = nh_.subscribe("/astra_registration", 10, &MultiAstraDriver::RegistrationCallback, this);
 }
